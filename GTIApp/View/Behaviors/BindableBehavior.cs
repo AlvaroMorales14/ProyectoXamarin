1a7ad8a11f4892161ff5d15acf7d46b25a109e85
Alvaro Morales <eckocr@gmail.com>
2019-08-21 18:23:27 -0600

    Corrigiendo el nombre de la clase del behavior que puse mal

---
+++ GTIApp/View/Behaviors/BindableBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace GTIApp.View.Behaviors
+{
+    public class BindableBehavior<T> : Behavior<T> where T : BindableObject
+    {
+        public T AssociatedObject { get; set; }
+        protected override void OnAttachedTo(T visualElement)
+        {
+            base.OnAttachedTo(visualElement);
+            AssociatedObject = visualElement;
+            if (visualElement.BindingContext != null)
+            {
+                BindingContext = visualElement.BindingContext;
+                //visualElement.BindingContextChanged
+            }
+        }
+    }
+}
